c0f7fca0b9434565f247696155168364a56253c5
allfather2005 <153357586+allfather2005@users.noreply.github.com>
2023-12-12 19:03:13 +0000

    Update services.nix

--- services.nix
+++ services.nix
@@ -1,15 +1,30 @@
 { config, pkgs, ... }:
 
-s=services
+{
+  services = {
+    printing.enable = true;
+    flatpak.enable = true;
+  };
 
-s = {
-  printing.enable = true;
-  flatpak.enable = true;
-  }
+  virtualisation = {
+    waydroid.enable = true;
+    podman = {
+      enable = true;
+    };
+  };
 
-virtualisation = {
-  podman = {
-    enable = true;
-    }
-  }
+  hardware = {
+    steam-hardware.enable = true;
+  };
 
+  programs = {
+    steam.enable = true;
+    direnv.enable = true;
+    neovim.enable = true;
+    git.enable = true;
+    adb.enable = true;
+    nano.enable = true;
+    neovim.withPython3 = true;
+    htop.enable = true;
+  };
+}
